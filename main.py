56963073c4a9f9927b3ee345182696b214ea0c61
Jan Pleszyński <jan.pleszynski.ext@cluepoints.com>
2025-02-20 10:59:07 +0100

    Add comments.

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from use_cases import use_case_1
 
 
 def main():
+    # Event bus configuration. This will be done in the app.py together with other setup code.
     event_bus = EventBus()
     event_bus.subscribe(EventTypes.EVENT_A, event_handler_a)
     event_bus.subscribe(EventTypes.EVENT_B, event_handler_b)
 
     # This will be normally invoked from the api
-    use_case_1(event_bus)
+    use_case_1(event_bus) # event_bus will be passed to the endpoints as fast_api dependency
 
 
 if __name__ == "__main__":
--- use_cases.py
+++ use_cases.py
@@ -4,6 +4,6 @@ from events import EventA, EventB
 def use_case_1(event_bus: EventBus):
     print("Running use case 1")
 
-    # Notify other parts of the system about that important thing
+    # Notify other parts of the system about important state changes that occurred in this use case.
     event_bus.publish(EventA(field_1="Hello", field_2=42))
     event_bus.publish(EventB(field_1="World", field_2=24, field_3=True))
